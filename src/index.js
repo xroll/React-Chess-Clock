985509bef501739849b47441a197fb37bf1f9ac3
xroll <github.xroll@gmail.com>
2020-11-07 18:00:18 +0100

    WIP: Steps/Timer/SetTimer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import ReactDOM, { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import './index.css';
+import logo from "./img/white-logo.png"
+import githubLogo from "./img/GitHub-Mark-Light-32px.png"
+import Step from "./Step";
 
 class App extends React.Component {
-    
+
+    constructor(props) {
+        super(props);
+        this.state = { 
+            step: 'main-menu',
+             };
+      }
+
     render(){
         return (
             <>
-               <h1>Page</h1>
+               <header> 
+                    <img id="logo" src={logo} alt="chess clock logo"/>
+                </header>
+
+                <main>
+                    <Step
+                        step={this.state.step}
+                    />            
+                </main>
+                
+                <footer>
+                    <a href="#"><img src={githubLogo} alt="github logo"/></a>
+                    <p>xroll - 2020</p>                
+                </footer> 
             </>
         );
     }
